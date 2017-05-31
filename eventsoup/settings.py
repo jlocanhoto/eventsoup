bfc871d3a5f9752637e3538fbc260e74b6e62cf7
Jailson Dias <jcd2@cin.ufpe.br>
2017-05-31 18:19:17 -0300

    bd

--- eventsoup/settings.py
+++ eventsoup/settings.py
@@ -155,8 +155,7 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10
 }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] =  dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
