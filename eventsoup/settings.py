09748a437675e0361cca2f63d2309fd1e65610f2
Jailson Dias <jcd2@cin.ufpe.br>
2017-05-31 18:27:59 -0300

    bd

--- eventsoup/settings.py
+++ eventsoup/settings.py
@@ -17,6 +17,8 @@ import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+
+db_from_ev = dj_database_url.config(conn_max_age=500)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -88,16 +90,16 @@ WSGI_APPLICATION = 'eventsoup.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-	    'default': {
-	        'ENGINE': 'django.db.backends.postgresql',
-	        'NAME': 'd6gs83dvv13b78',
-	        'USER': 'hqqduloesglecc',
-	        'PASSWORD': 'ac57e896d6882b5cf955b7c625d9af109d0be42fd5fec05f5f2b300b959e852a',
-	        'HOST': 'ec2-54-225-68-71.compute-1.amazonaws.com',
-	        'PORT': '5432',
-	    }
-	}
+# DATABASES = {
+# 	    'default': {
+# 	        'ENGINE': 'django.db.backends.postgresql',
+# 	        'NAME': 'd6gs83dvv13b78',
+# 	        'USER': 'hqqduloesglecc',
+# 	        'PASSWORD': 'ac57e896d6882b5cf955b7c625d9af109d0be42fd5fec05f5f2b300b959e852a',
+# 	        'HOST': 'ec2-54-225-68-71.compute-1.amazonaws.com',
+# 	        'PORT': '5432',
+# 	    }
+# 	}
 
 
 # Password validation
@@ -139,6 +141,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
+
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
@@ -155,7 +158,10 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10
 }
 
-DATABASES['default'] =  dj_database_url.config()
+# DATABASES['default'] =  dj_database_url.config()
+
+DATABASES = { 'default': db_from_ev,
+            }
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
