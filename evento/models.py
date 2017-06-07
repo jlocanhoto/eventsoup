4e85a152280efaff56fa059bb2d0044103209d19
Marcos Barreto <msb5@cin.ufpe.br>
2017-06-07 01:33:11 -0300

    ajustes em evento (perdoa os commits gigantes)

--- evento/models.py
+++ evento/models.py
@@ -1,18 +1,20 @@
 from django.db import models
 from autoslug import AutoSlugField
 from usuarios.models import Usuario
+from pacotes.models import Pacote
 
 class Evento(models.Model):
     nome = models.CharField('Evento', max_length=200)
     quantidade_pessoas = models.PositiveIntegerField('Quantidade de Pessoas')
     data = models.DateTimeField('Data do Evento')
-    local = models.CharField('Local do Evento', max_length = 200)
     RESTRICOES = (('vegetariano', 'Vegetariano'),
                   ('regional', 'Regional'))
     restricoes = models.CharField('Tipos de Alimentos', choices=RESTRICOES, max_length = 50)
     orcamento = models.FloatField('Orçamento para o Evento')
-    slug = AutoSlugField('Slug', populate_from='nome', unique_with=('data'), unique=True)
+    slug = AutoSlugField('Slug', populate_from='nome', always_update=True, unique_with=('data'), unique=True)
     criador = models.ForeignKey(Usuario, verbose_name = 'Criador', related_name = 'eventos')
+
+    pacotes = models.ManyToManyField(Pacote, verbose_name='Pacotes do Evento', related_name = 'pacotes')
 
     class Meta:
         verbose_name = 'Evento'
@@ -20,3 +22,16 @@ class Evento(models.Model):
 
     def __str__(self):
         return str(self.nome) or str(self.cpf_cnpj) or str(self.email)
+
+class Endereco(models.Model):
+    rua = models.CharField('Rua', max_length = 250)
+    bairro = models.CharField('Bairro', max_length = 100)
+    cidade = models.CharField('Cidade', max_length = 50)
+    estado = models.CharField('Estado', max_length = 20)
+    cep = models.CharField('CEP', max_length = 15)
+    numero = models.CharField('Número', max_length = 10)
+    evento = models.ForeignKey(Evento, verbose_name='Local do evento', related_name = 'endereco', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Endereço'
+        verbose_name_plural = 'Endereços'
--- evento/permissions.py
+++ evento/permissions.py
@@ -1,5 +1,7 @@
 from rest_framework.permissions import BasePermission
 
+from .models import Evento
+
 class EventoPermission(BasePermission):
 
     def has_permission(self, request, view):
@@ -7,13 +9,34 @@ class EventoPermission(BasePermission):
             return True
         if request.method == 'POST':
             return request.user and request.user.is_authenticated
-        if request.method in ('GET', 'HEAD', 'OPTIONS'):
+        if request.method in ('GET','HEAD','OPTIONS'):
             return True
         if request.method in ('PUT', 'DELETE','PATCH'):
-            return request.user and (request.user.is_staff or request.user.cpf_cnpj == view.get_object().criador.cpf_cnpj)
+            try:
+                return request.user and (request.user.is_staff or request.user.cpf_cnpj == view.get_object().criador.cpf_cnpj)
+            except:
+                return False
         return False
-
 
 class ListOwnerEventosPermission(BasePermission):
     def has_permission(self, request, view):
         return request.user and request.user.is_authenticated
+
+class EnderecoPermission(BasePermission):
+
+    def has_permission(self, request, view):
+        if request.user.is_staff:
+            return True
+        if request.method in ('POST','GET','HEAD','OPTIONS'):
+            try:
+                evento = Evento.objects.get(slug=view.kwargs['slug_evento'])
+                print(evento.criador.cpf_cnpj == request.user.cpf_cnpj)
+                return request.user and (request.user.is_staff or request.user.cpf_cnpj == evento.criador.cpf_cnpj)
+            except:
+                return False
+        if request.method in ('PUT','DELETE','PATCH'):
+            try:
+                return request.user and (request.user.is_staff or request.user.cpf_cnpj == view.get_object().evento.criador.cpf_cnpj)
+            except:
+                return False
+        return False
