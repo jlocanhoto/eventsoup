c6adba04ec6187bd527b4a9ed2fd34f28772f62f
Jailson Dias <jcd2@cin.ufpe.br>
2017-06-07 23:36:49 -0300

    add o corsheaders

--- eventsoup/settings.py
+++ eventsoup/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'rest_framework_jwt',
+    'corsheaders',
 
     'usuarios',
     'evento',
@@ -58,6 +59,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'eventsoup.urls'
