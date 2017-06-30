c2e97ca8c222742e72b6600dc6a2e1a216544b92
Marcos Barreto <msb5@cin.ufpe.br>
2017-06-30 19:48:36 -0300

    remocao da rota de criacao do pacote e itempacote

--- pacotes/serializers.py
+++ pacotes/serializers.py
@@ -13,12 +13,12 @@ class PacoteSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Pacote
-        fields = ('id','slug', 'nome','quantidade_pessoas','restricoes','preco','dono','codigo')
-        read_only_fields = ('id','slug','dono','codigo')
+        fields = ('id','slug', 'nome','quantidade_pessoas','restricoes','preco','dono','codigo','codigo_pag_seguro')
+        read_only_fields = ('id','slug')
 
 class ItemPacoteSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ItemPacote
         fields = ('slug', 'item','quantidade_item','pacote')
-        read_only_fields = ('slug','pacote')
+        read_only_fields = ('slug',)
--- pacotes/urls.py
+++ pacotes/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 router = routers.DefaultRouter()
 router.register(r'crud-itens', views.ItemViewSet)
-router.register(r'crud-pacotes', views.PacoteViewSet)
-router.register(r'crud-item-pacote/(?P<slug_pacote>[^/.]+)', views.ItemPacoteViewSet)
+# router.register(r'crud-pacotes', views.PacoteViewSet)
+# router.register(r'crud-item-pacote/(?P<slug_pacote>[^/.]+)', views.ItemPacoteViewSet)
 
 urlpatterns = [
     url(r'list-all-pacotes/$', views.ListPacotes.as_view(),name = 'list-all-pacotes'),
